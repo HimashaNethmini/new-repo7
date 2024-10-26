e7101d6a387db7fceaa5e106682257893460cd45
Himashaa <himashanethmini05@gmail.com>
2024-10-26 22:25:33 +0530

    Creating the todoListItem component

---
+++ src/todos/todoList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoListItem from './TodoListItem';
+
+const TodoList = ( {todos} ) => {
+    return (
+        <div className='list-wrapper'>
+            {todos.map(todo => <TodoListItem todo={todo} />)}
+        </div>
+    )
+}
+
+export default TodoList
