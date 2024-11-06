02fcb2d6f8b1db1b5cb9dcaf7e3b358a51a19eba
Himashaa <himashanethmini05@gmail.com>
2024-11-06 16:44:29 +0530

    Redux DevTools

--- src/store.js
+++ src/store.js
@@ -1,20 +1,25 @@
-import { createStore, combineReducers } from 'redux';
-import { todos } from './todos/reducers'
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'
-import persistReducer from 'redux-persist/es/persistReducer';
-import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
+import { createStore, combineReducers } from "redux";
+import { todos } from "./todos/reducers";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
 
 const reducers = {
-    todos,
-}
+  todos,
+};
 
-const persistConfig ={
-    key: 'root',
-    storage,
-    stateReconciler: autoMergeLevel2
-}
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
 
 const rootReducer = combineReducers(reducers);
-const persistReducer = persistReducer(persistConfig, rootReducer)
-export const configureStore = () => createStore(rootReducer);
+const persistReducer = persistReducer(persistConfig, rootReducer);
+export const configureStore = () => 
+    createStore(
+        persistReducer,
+        window._REDUX_DEVTOOLS_EXTENSION_&&
+        window._REDUX_DEVTOOLS_EXTENSION_(),
+);
