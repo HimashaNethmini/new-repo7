94100c6d014d996dc49616486146b25c84cd4e25
Himashaa <himashanethmini05@gmail.com>
2024-11-06 10:37:11 +0530

    Adding Redux to a React app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import App from "./App.js";
 import ReactDOM from "react-dom/client";
+import { Provider } from 'react-redux'
+import { configureStore } from './store.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(
+    <Provider store={configureStore()}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+)
